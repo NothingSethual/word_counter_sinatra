cd926cc14e3bee72d7eca867b65b00fa43a88fe5
Garrett Olson <garrettdolson@gmail.com>
2015-04-24 10:41:33 -0700

    Takes into account punctuation and capitalization.

--- lib/word_counter.rb
+++ lib/word_counter.rb
@@ -1,10 +1,13 @@
 class String
   define_method(:get_count) do |target|
-    phrase_words = self.split()
+
+    # Downcase original phrase, remove all punctuation
+    phrase_stripped = self.downcase().gsub(/[^a-z0-9\s]/i, '')
+    phrase_words = phrase_stripped.split()
     target_count = 0
 
     phrase_words.each do |word|
-      if word.==(target)
+      if word.==(target.downcase())
         target_count = target_count + 1
       end
     end
--- spec/word_counter_spec.rb
+++ spec/word_counter_spec.rb
@@ -5,4 +5,16 @@ describe('String#get_count') do
   it('returns the count when no matches are found') do
     expect('I like cats'.get_count('dogs')).to(eq(0))
   end
+
+  it('returns the count when one match is found ') do
+    expect('I like cats'.get_count('cats')).to(eq(1))
+  end
+
+  it 'return the count when multiple matches are found' do
+    expect('I like cats cats'.get_count('cats')).to(eq(2))
+  end
+
+  it 'returns the count if capitalization is in effect (ignores capitalization)' do
+    expect('Cats are amazing'.get_count('cats')).to(eq(1))
+  end
 end
