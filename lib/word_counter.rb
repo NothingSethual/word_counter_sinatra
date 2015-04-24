653588ddd0d8ff2cd176e310097462200e50f88a
Garrett Olson <garrettdolson@gmail.com>
2015-04-24 12:00:08 -0700

    Refactor word_counter logic, add Heroku link to README

--- lib/word_counter.rb
+++ lib/word_counter.rb
@@ -1,5 +1,6 @@
 class String
   define_method(:get_count) do |target|
+    target.downcase!
 
     # Downcase original phrase, remove all punctuation
     phrase_stripped = self.downcase().gsub(/[^a-z0-9\s]/i, '')
@@ -7,7 +8,9 @@ class String
     target_count = 0
 
     phrase_words.each do |word|
-      if word.==(target.downcase())
+      # If the phrase word matches the target word
+      if word.==(target)
+        # Increment target_count by one
         target_count = target_count + 1
       end
     end
